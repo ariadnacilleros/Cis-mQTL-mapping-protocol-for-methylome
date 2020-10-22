953c4d9f21c3ed3ac6c7e005061ec875a5d8e960
ariadnacilleros <43587721+ariadnacilleros@users.noreply.github.com>
2020-10-22 16:25:29 +0200

    Delete Covariates.R

--- Covariates.R
+++
@@ -1,8 +0,0 @@
-sex <- read.csv("Final_imp.sexcheck", sep="")
-
-df$FID <- paste(df$FID,df$FID, sep = "_")
-
-df_t <- t(df)
-rownames(df_t)[1] <-("id")
-
-write.table(x = df_t, file = "COV_imp.txt", sep = "\t", quote = F, col.names = F)
