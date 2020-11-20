2fd7f52180704167554213a67fceb6078198cf5f
ariadnacilleros <43587721+ariadnacilleros@users.noreply.github.com>
2020-11-20 10:59:04 +0100

    Update covariates.R

--- covariates.R
+++ covariates.R
@@ -1,4 +1,6 @@
-# Create covariates text file
+# This script creates covariates text file, but you will need to adapt it 
+# to your inputs
+
 subs <- read.csv('example.csv', sep = ';')
 subs$sex[subs$sex == 1] <- 2  #change female code for PLINK
 subs$sex[subs$sex == 0] <- 1  #change maele code for PLINK
