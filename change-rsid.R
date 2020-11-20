97636f804affc0abb4f97f2dd1a3ad9dda3ee793
ariadnacilleros <43587721+ariadnacilleros@users.noreply.github.com>
2020-11-20 10:22:32 +0100

    Update change-rsid.R

--- change-rsid.R
+++ change-rsid.R
@@ -4,13 +4,12 @@
 
 # OVERRIDES .BIM FILES
 
+arg <- commandArgs(trailingOnly = T)
+
 cat('\nExecuting change-rsid.R script.')
 
-setwd('inter/')
-names <- c('example.bim') #CHANGE FILE'S NAME
-
 # List of dataframes
-myfiles <- lapply(names, read.delim, header = F)
+myfiles <- lapply(arg, read.delim, header = F)
 
 # This section converts Variant ID column from a PLINK's map file
 # to "chr':position" format, using chromosome and position columns
@@ -27,9 +26,9 @@ res <- lapply(myfiles, chrpos) # result
 cat('\n')
 
 for (i in 1:length(res)) {
-  fwrite(res[[i]], names[i], quote = F, sep = '\t',
+  fwrite(res[[i]], arg[i], quote = F, sep = '\t',
          row.names = F, col.names = F)
-  cat('File', names[i], 'has been overwritten.\n')
+  cat('File', arg[i], 'has been overwritten.\n')
 }
 
 cat('\n')
