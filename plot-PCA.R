51e9b34605ef0fd9e3a3a4d6ded79d7d00bc186c
ariadnacilleros <43587721+ariadnacilleros@users.noreply.github.com>
2021-02-08 10:00:27 +0100

    Update plot-PCA.R

--- plot-PCA.R
+++ plot-PCA.R
@@ -1,29 +1,34 @@
-#This filw will plot the Prinipal Components by different 
-# pheno traits: sex, ethnicity, cohort.
-# You will need to adapt the script considering your inputs. 
+#Objective: Plot the Prinipal Components by different pheno traits: sex, ethnicity, cohort.
+#Version: 02-08-2021
+#Contact: acilleros001@ikasle.ehu.eus
 
 library(dplyr)
 library(ggplot2)
 library(data.table)
 
-df1 <- fread('example.csv', sep = ';') #we need a file with first two columns FID and IID, followed by pheno data (sex,age...)
+#Step 1: Read a a file with first two columns FID and IID, followed by pheno data (sex,age...)
+df1 <- fread('example.csv', sep = ';') 
 
-recode1 <- recode(df1$sex,'0' = 'Male', '1' = 'Female') #we make sure that Male = 0 and Female = 1 according to PLINK
+#Step 2: Make sure that Male = 0 and Female = 1 according to PLINK
+recode1 <- recode(df1$sex,'0' = 'Male', '1' = 'Female') 
 df1[,sex:=recode1] 
-recode2 <- recode(df1$ethnic_origin, '2' = 'Asiatic', '3' = 'Black', '4' = 'Arabe', #change codes for ethnic origin
+
+#Step 3: It is not necessary, but we changed the ethnic origin numbers by explanatory names
+recode2 <- recode(df1$ethnic_origin, '2' = 'Asiatic', '3' = 'Black', '4' = 'Arabe', 
                   '5' = 'Gipsy', '6' = 'American indios', '7' = 'Others',
                   '11' = 'Both parents white and Spanish',
                   '12' = 'Both parents white, not Spanish, but European',
                   '13' = 'Both parents white, but at least one not European')
 df1[,ethnic_origin:=recode2]
 
-pcs <- fread('clean-PIHAT-prunned.eigenvec') #upload PCs data
+#Step 4: Upload PCs data
+pcs <- fread('clean-PIHAT-prunned.eigenvec') 
 names(pcs) <- c('FID', 'IID', paste0('PC', 1:20)) #change colum names
 
-p <- merge(df1, pcs, all.y = T) #merge both dataframes: pheno data + PCs
+#Step 5: Merge both dataframes (pheno data + PCs)
+p <- merge(df1, pcs, all.y = T) 
 
 #Plot PCs and pheno variables 
-
 pdf('PIHAT-clean-PC1vsPC2-cohort.pdf')
 qplot(PC1, PC2, colour = cohort8_name,
       data = p, alpha = 0.2) + theme_light()
