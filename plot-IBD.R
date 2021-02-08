b8243762c1a118974d38ebc89a9e09c1185604f1
ariadnacilleros <43587721+ariadnacilleros@users.noreply.github.com>
2021-02-08 09:43:24 +0100

    Update plot-IBD.R

--- plot-IBD.R
+++ plot-IBD.R
@@ -1,9 +1,12 @@
-#This script will plot the IBD and write a text file
-#with the name of the samples with a PI-HAT > 0.18
+#Objective: Plot the IBD and write a text file with the name of the samples with a PI-HAT > 0.18
+#Version: 08-02-2021
+#Contact: acilleros001@ikasle.ehu.eus
 
+#Step 1: Read qc/wg-updated-IBD.genome file
 arg <- commandArgs(trailingOnly = T)
 data <- read.table(paste0(arg, '.genome'), header = T)
 
+#Step 2: plot histogram
 namepdf <- paste0(arg,'-hist.pdf')
 pdf(namepdf)
 hist(
@@ -11,6 +14,7 @@ hist(
   xlab = 'Estimated mean pairwise IBD', main = '')
 dev.off()
 
+#Step 3: elavorate a list if samples with a PI-HAT > 0.18
 out <- data[data$PI_HAT > 0.18,]
 nametable <- paste0(arg, '-fail-IBD-check.txt')
 write.table(out, nametable, sep = '\t', quote = F, row.names = F)
