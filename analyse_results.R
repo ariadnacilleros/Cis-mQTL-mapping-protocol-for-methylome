b237226b3360cc92083ecc7578793e1e1fe273c4
ariadnacilleros <43587721+ariadnacilleros@users.noreply.github.com>
2021-02-18 12:53:02 +0100

    Update analyse_results.R

--- analyse_results.R
+++ analyse_results.R
@@ -6,7 +6,7 @@
 setwd("TensorQTL/")
 
 #Step 1: Read results text file
-results <- read.table("cis_tensorQTL_maf05_PC5_sex_INMA.txt", header = T, sep = "\t")
+results <- read.table("cis_tensorQTL_maf05_PC5_sex_INMA_18022021.txt", header = T, sep = "\t")
 
 #Step 2: Adjust by multiple-testing (Bonferroni)
 results$Bonferroni <- p.adjust(results$pval_beta, method = "bonferroni")
@@ -20,7 +20,7 @@ var <- read.table(file = "../EPIC/all_cpg_variances.txt", header = T, sep = "\t"
 
 #Step 3.1: Write a text file with the variance of each CpG
 df_cpgs <- var[var$Row.names %in% results$phenotype_id, ]
-write.table(x = df_cpgs, file = "variance_table_cpgs_INMA.txt", 
+write.table(x = df_cpgs, file = "variance_table_cpgs_INMA_18022021.txt", 
             sep = "\t", dec = ".", row.names = F, col.names = T, quote = F)
 
 #Step 4: Obtain the number of counts from the significant SNPs from the freqx filr (whole_genome_definitive folder)
@@ -32,6 +32,6 @@ counts <- read.table(file = "../whole_genome_definitive/whole_genome_maf05_filt_
 #the major allele (should be A2) and the homozigous minor allele counts (C.HOM.A1.)
 df_snps <- counts[counts$SNP %in% results$variant_id, c("SNP", "A1","A2","C.HOM.A1.") ]
 df_snps <- merge(x = results[,c("variant_id", "maf")], y = df_snps, by.x = "variant_id", by.y = "SNP")
-write.table(x = df_snps, file = "maf_counts_table_snps_INMA.txt", 
+write.table(x = df_snps, file = "maf_counts_table_snps_INMA_18022021.txt", 
             sep = "\t", dec = ".", row.names = F, col.names = T, quote = F)
 
